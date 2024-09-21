4c61145d5d514c0eaf3ccc809e86b5fd045816b2
Arshad1998 <arshadshafeeq09@gmail.com>
2024-09-21 20:54:44 +0530

    September 15 files included

---
+++ src/test/java/ex_15092024/CRUD/POST/NonBDDStylePOST.java
@@ -0,0 +1,37 @@
+package ex_15092024.CRUD.POST;
+
+import io.restassured.RestAssured;
+import io.restassured.http.ContentType;
+import io.restassured.response.Response;
+import io.restassured.response.ValidatableResponse;
+import io.restassured.specification.RequestSpecification;
+
+public class NonBDDStylePOST {
+
+    public static void main(String[] args) {
+
+//How Payload is given in Rest Assured
+        //  String(1%)
+        //  Hashmap(4%)
+        //  Classes( 95%)
+
+        String Payload = "     {\n" +
+                "        \"username\" : \"admin\",\n" +
+                "        \"password\" : \"password123\"\n" +
+                "    }";
+
+        RequestSpecification request = RestAssured.given();
+                request.baseUri("https://restful-booker.herokuapp.com");
+                request.basePath("/auth");
+                request.contentType(ContentType.JSON);
+                request.body(Payload);
+//                request.log().all();
+
+
+        Response response = request.when().post();
+
+        ValidatableResponse validation = response.then();
+                validation.log().all();
+                validation.statusCode(200);
+    }
+}
